08f6c25908d071f3c8137d0b36c9614d17a3f4a2
Nana Chuto <mail@nanachuto.com>
2023-05-05 16:33:54 +0200

    done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function SearchBar() {
+const SearchBar = ({ searchQuery, onChangeInput }) => {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={onChangeInput}
+      />
       <button type="submit">Search</button>
     </form>
   );
-}
+};
 
 export default SearchBar;
